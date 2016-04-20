6830f8a567998ffe306f2280ad6e0cef214fbbec
Ulysses Tinajero <utinajero07@gmail.com>
2016-04-20 09:33:09 -0700

    check this 3

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint', 'gulp-jshint'],);
+  grunt.registerTask('default', ['jshint', 'gulp-jshint']);
 
 };
